18f652d793b7a032d189d6939f36def07894dc7b
Jairaj Sethi <j@jairaj.org>
2013-07-03 05:12:03 -0400

    rewrite stalker/watcher combo

--- server/watcher.js
+++ server/watcher.js
@@ -1,135 +1,121 @@
-var fs      = require('fs'),
-	path    = require('path'),
-	stalker = require(__dirname + '/stalker');
+var fs   = require('fs'  ),
+	path = require('path');
 
-var MAX_FILES = 1;
+var watched  = {},
+	handles  = {},
+	maxFiles = 2000;
 
-var numFiles = 0;
-
-
-
-exports.watch = function (dir, callback) {
-	var files = {};
-
-	if (dir[dir.length-1] === '/') {
-		dir = dir.substr(0, dir.length-1);
-	}
-
-	findSync(dir).forEach(function (fileName) {
-		setupWatcher(fileName);
-	});
-
-	stalker.watch(
-		dir,
-		function (fileName) {
-			setupWatcher(fileName);
-			changeDetected(fileName);
-		},
-		function (fileName) {
-			destroyWatcher(fileName);
-			changeDetected(fileName);
-		}
-	);
-
-	function setupWatcher (fileName) {
-		if (numFiles >= MAX_FILES) {
-			return;
-		}
-
-		if ( files[fileName] ) {
-			return;
-		}
-
-		for (var parts=fileName.split('/'), i=0, l=parts.length; i<l; i++) {
-			if (parts[i][0] === '.') {
-				return;
-			}
-		}
-
-		var watcher;
-		if (fs.watch) {
-			watcher = fs.watch(fileName, function () {
-				changeDetected(fileName);
-			});
-		}
-		else {
-			fs.watchFile(fileName, function () {
-				changeDetected(fileName);
-			});
-			watcher = true;
-		}
-
-		files[fileName] = watcher;
-		numFiles++;
-	}
-
-	function destroyWatcher (fileName) {
-		var watcher = files[fileName];
-		if ( !watcher ) {
-			return;
-		}
-
-		if (watcher !== true) {
-			watcher.close();
-		}
-		else {
-			fs.unwatchFile(fileName);
-		}
-
-		delete files[fileName];
-		numFiles--;
-	}
-
-	function changeDetected (fileName) {
-		callback(fileName);
-	}
+exports.watch = function (reqPath, onChange) {
+	watchFolder(path.resolve(reqPath), onChange);
 };
 
 
 
-function findSync (dir) {
-	var inodes    = {},
-		files     = [],
-		fileQueue = [];
-
-	prepopulateQueue();
-	processQueue();
-	return files;
-
-	function prepopulateQueue () {
-		var stat = fs.lstatSync(dir);
-		if ( stat.isDirectory() ) {
-			fs.readdirSync(dir).forEach(function (f) {
-				fileQueue.push(path.join(dir, f));
-			});
-		}
-		else {
-			fileQueue.push(dir);
-		}
+function watchFolder (folderPath, onChange, shouldPrune, noReset) {
+	if (!noReset && handles[folderPath]) {
+		handles[folderPath].close();
 	}
 
-	function processQueue () {
-		var file, stat;
+	fs.stat(folderPath, function (err, stats) {
+		if (err || (Object.keys(handles).length >= maxFiles)) {
+			return;
+		}
 
-		while (fileQueue.length) {
-			if (files.length >= MAX_FILES) {
+		if ( stats.isFile() ) {
+			if ( handles[folderPath] ) {
+				handles[folderPath].close();
+			}
+			try {
+				handles[folderPath] = fs.watch(folderPath, function () {
+					onChange(folderPath);
+				});
+			} catch (err) {
+				maxFiles = Object.keys(handles).length;
+			}
+			addFile(folderPath, stats);
+			onChange(folderPath);
+			return;
+		}
+		else if ( !stats.isDirectory() ) {
+			return;
+		}
+
+		addFile(folderPath, stats);
+
+		if ( !noReset ) {
+			try {
+				handles[folderPath] = fs.watch(folderPath, function (evt) {
+					watchFolder(folderPath, onChange, true, (evt === 'change'));
+				});
+			} catch (err) {
+				maxFiles = Object.keys(handles).length;
+			}
+		}
+
+		fs.readdir(folderPath, function (err, files) {
+			if (err) {
 				return;
 			}
+			files.forEach(function (file) {
+				if (file[0] === '.') {
+					return;
+				}
+				var fPath = path.join(folderPath, file);
+				watchFolder(fPath, onChange);
+			});
+		});
 
-			file = fileQueue.shift();
-			stat = fs.lstatSync(file);
-			if ( inodes[stat.ino] ) {
-				continue;
+		if (shouldPrune) {
+			pruneDir(folderPath, onChange);
+		}
+	});
+}
+
+function addFile (fPath, stats) {
+	var dir  = path.dirname(fPath),
+		base = path.basename(fPath);
+
+	if ( !watched[dir] ) {
+		watched[dir] = {};
+	}
+	if ( !watched[dir].files ) {
+		watched[dir].files = {};
+	}
+	watched[dir].files[base] = stats.mtime.valueOf();
+}
+
+function pruneDir (dir, onChange, force) {
+	if (!watched[dir] || !watched[dir].files) {
+		return;
+	}
+
+	Object.keys(watched[dir].files).forEach(function (file) {
+		var fPath = path.join(dir, file);
+
+		if (force) {
+			pruneFile(fPath, file);
+		}
+		else {
+			fs.stat(fPath, function (err, stats) {
+				if (err) {
+					pruneFile(fPath, file);
+				}
+			});
+		}
+	});
+
+	function pruneFile (fPath, file) {
+		if ( watched[fPath] ) {
+			pruneDir(fPath, onChange, true);
+			delete watched[fPath];
+		}
+		else {
+			if ( handles[fPath] ) {
+				handles[fPath].close();
+				delete handles[fPath];
 			}
-
-			inodes[stat.ino] = true;
-			files.push(file);
-
-			if ( stat.isDirectory() ) {
-				fs.readdirSync(file).forEach(function (f) {
-					fileQueue.push(path.join(file, f));
-				});
-			}
+			delete watched[dir].files[file];
+			onChange(fPath);
 		}
 	}
 }
