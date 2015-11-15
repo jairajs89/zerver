34b51f320a1167e8f2a7998d428441be2dd2f051
Jairaj <j@jairaj.org>
2015-11-15 06:41:22 -0500

    only turn on websocket stream if necessary

--- server/slave.js
+++ server/slave.js
@@ -11,42 +11,50 @@ function Slave(options) {
 	var self = this;
 	self.options = extend({}, options || {});
 
-	self.streams = new EventEmitter();
-	self.streams.list = [];
-	self.streams.on('connection', function (stream) {
-		stream.on('_message', function (data) {
-			if (data.type === 'log') {
-				try {
-					process.send({ log: (data.level+': '+data.message) });
-				} catch (err) {}
-			}
+	var needsStream = options.cli || options.refresh;
+
+	if (needsStream) {
+		self.streams = new EventEmitter();
+		self.streams.list = [];
+		self.streams.on('connection', function (stream) {
+			stream.on('_message', function (data) {
+				if (data.type === 'log') {
+					try {
+						process.send({ log: (data.level+': '+data.message) });
+					} catch (err) {}
+				}
+			});
 		});
-	});
+	}
 
 	var zerver = new (require(__dirname+path.sep+'zerver'))(self.options, function () {
 		process.send({ started: true });
-		process.nextTick(function () {
-			try {
-				new (getWebsocketServer())({ httpServer: zerver._app })
-					.on('request', function (req) {
-						var conn = req.accept('zerver-debug', req.origin);
-						self.handleRequest(conn);
-					});
-			} catch (err) {
-				console.error('failed to init debug channel');
-			}
-		});
-	});
-
-	process.on('message', function (data) {
-		if (data) {
-			if (data.debugRefresh) {
-				self.refresh();
-			} else if (data.cli) {
-				self.cliRequest(data.cli);
-			}
+		if (needsStream) {
+			process.nextTick(function () {
+				try {
+					new (getWebsocketServer())({ httpServer: zerver._app })
+						.on('request', function (req) {
+							var conn = req.accept('zerver-debug', req.origin);
+							self.handleRequest(conn);
+						});
+				} catch (err) {
+					console.error('failed to init debug channel');
+				}
+			});
 		}
 	});
+
+	if (needsStream) {
+		process.on('message', function (data) {
+			if (data) {
+				if (data.debugRefresh) {
+					self.refresh();
+				} else if (data.cli) {
+					self.cliRequest(data.cli);
+				}
+			}
+		});
+	}
 }
 
 Slave.prototype.refresh = function () {
